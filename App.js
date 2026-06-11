dfa3580c9c1e52c03101831d38008281c8738271
AhayLuay <arifviansyah28@.com>
2026-06-11 19:10:37 +0700

    style edit

--- App.js
+++ App.js
@@ -3,18 +3,60 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: 'white' }}>
+
+      <View style={styles.container}>
+        <Text> Container 1 </Text>
+      </View>
+
+      <View style={styles.container2}> 
+       <Text> Container 2 </Text>
+      </View>
+
+      <View style={styles.container3}> 
+       <Text> Container 3 </Text> 
+      </View>
+    
+      <View style={[styles.containerDefault, { backgroundColor: '#ff8fff',
+                                              marginTop: 20,
+                                              marginLeft: 20,}]}>
+        <Text> Container 4 </Text>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: 200,
+    height: 100,
+    backgroundColor: '#ff8f8f',
+    marginTop: 50,
+    marginBottom: 20,
+    marginLeft: 20,
+    padding: 10,
+    borderRadius: 20,
+    borderWidth: 5, borderColor: '#9f5959',
+    opacity: 0.4,
   },
+  container2: {
+    width: 200,
+    height: 100,
+    backgroundColor: '#8f8fff',
+    marginBottom: 20,
+    marginLeft: 20,
+    padding: 10,
+  },
+  container3: {
+    width: 200,
+    height: 100,
+    backgroundColor: '#93ff93',
+    marginLeft: 20,
+    padding: 10,
+  },
+  containerDefault: {
+    width: 200,
+    height: 100,
+  }
 });
