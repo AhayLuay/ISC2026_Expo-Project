b51208bb43065addeaa43ee55b7cb957a8ba8550
AhayLuay <arifviansyah28@.com>
2026-06-12 17:10:52 +0700

    style edit

--- App.js
+++ App.js
@@ -3,25 +3,20 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: 'white' }}>
+    <View style={styles.container}>
 
-      <View style={styles.container}>
-        <Text> Container 1 </Text>
-      </View>
-
-      <View style={styles.container2}> 
-       <Text> Container 2 </Text>
-      </View>
-
-      <View style={styles.container3}> 
-       <Text> Container 3 </Text> 
+      <View style={styles.box}>
+      <Text>1</Text>
       </View>
     
-      <View style={[styles.containerDefault, { backgroundColor: '#ff8fff',
-                                              marginTop: 20,
-                                              marginLeft: 20,}]}>
-        <Text> Container 4 </Text>
+      <View style={styles.box}>
+
+
       </View>
+        <View style={styles.box}>
+
+      </View>
+
 
     </View>
   );
@@ -29,34 +24,23 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    width: 200,
-    height: 100,
-    backgroundColor: '#ff8f8f',
-    marginTop: 50,
+    backgroundColor: 'gray',
+    flex: 1,
+    marginTop: 35,
+    marginBottom: 40,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'flex-start'
+  },
+  box: {
+    backgroundColor: 'skyblue',
+    width: 30,
+    height: 10,
     marginBottom: 20,
-    marginLeft: 20,
-    padding: 10,
-    borderRadius: 20,
-    borderWidth: 5, borderColor: '#9f5959',
-    opacity: 0.4,
-  },
-  container2: {
-    width: 200,
-    height: 100,
-    backgroundColor: '#8f8fff',
-    marginBottom: 20,
-    marginLeft: 20,
-    padding: 10,
-  },
-  container3: {
-    width: 200,
-    height: 100,
-    backgroundColor: '#93ff93',
-    marginLeft: 20,
-    padding: 10,
-  },
-  containerDefault: {
-    width: 200,
-    height: 100,
+    marginHorizontal: 10,
+    padding: 20,
+    borderRadius: 15,
+    borderColor: '#004b76',
+    borderWidth: 5
   }
 });
